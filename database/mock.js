62c943b42a2f379e55cbf3af7a885086f94db59b
Nick Peters <nick@barbetsapp.com>
2016-10-06 15:13:54 -0600

    update seed file

--- database/mock.js
+++ database/mock.js
@@ -1,46 +1,76 @@
 const trips = [
   {
-    tripID: '123',
+    tripID: '101',
     accountID: 1,
-    name: 'Tuscany Mar 10, 2017',
+    name: 'Tuscany Jan 01, 2017',
     status: 'active',
     flightInfo: [
       {
         airline: 'American',
         arrivalTime: '10:40 AM',
         departureTime: '12:30 PM',
-        arrivalDate: '03-10-2017',
-        departureDate: '03-17-2017'
+        arrivalDate: '2017-01-01',
+        departureDate: '2017-01-04'
       }
     ]
   },
   {
-    tripID: '456',
+    tripID: '102',
+    accountID: 1,
+    name: 'Bali Feb 01, 2017',
+    status: 'active',
+    flightInfo: [
+      {
+        airline: 'Lufthansa',
+        arrivalTime: '10:40 AM',
+        departureTime: '12:30 PM',
+        arrivalDate: '2017-02-01',
+        departureDate: '2017-02-05'
+      }
+    ]
+  },
+  {
+    tripID: '201',
     accountID: 2,
-    name: 'Sri Lanka Dec 15, 2016',
+    name: 'Bali May 01, 2017',
     status: 'active',
     flightInfo: [
       {
         airline: 'United',
         arrivalTime: '11:40 PM',
         departureTime: '09:30 AM',
-        arrivalDate: '12-15-2016',
-        departureDate: '12-25-2016'
+        arrivalDate: '2017-05-01',
+        departureDate: '2017-05-04'
       }
     ]
   },
   {
-    tripID: '789',
-    accountID: 3,
-    name: 'Phuket July 03, 2017',
+    tripID: '202',
+    accountID: 2,
+    name: 'Phuket Dec 01, 2016',
     status: 'active',
     flightInfo: [
       {
         airline: 'Southwest',
         arrivalTime: '01:40 PM',
         departureTime: '4:15 PM',
-        arrivalDate: '07-03-2017',
-        departureDate: '07-10-2017'
+        arrivalDate: '2016-12-01',
+        departureDate: '2016-12-05'
+      }
+    ]
+  },
+  {
+    tripID: '301',
+    accountID: 3,
+    name: 'Phuket Jan 15, 2017',
+    status: 'active',
+    flightInfo: [
+      {
+        airline: 'Southwest',
+        arrivalTime: '01:40 PM',
+        departureTime: '4:15 PM',
+        arrivalDate: '2017-01-15',
+        departureDate: '2017-01-19'
       }
     ]
   }
