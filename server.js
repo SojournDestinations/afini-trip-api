361cd8c9614a6c3d2995a3814a61ef564df1bea4
Nick Peters <nick@barbetsapp.com>
2016-10-10 15:34:32 -0600

    add express middleware for server.js

--- server.js
+++ server.js
@@ -30,6 +30,13 @@ healthcheckRouter.route('/healthbad').get((req, res) => {
   someUnknownObject.produceError()
 })
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  next()
+})
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use('/trips', router)
